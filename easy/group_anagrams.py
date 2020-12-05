42cc1e5dac21abd71251cf4fe3e1f7283378a35a
Matthew James Wong <matthew.wong76@gmail.com>
2020-12-04 20:15:27 -0800

    Create group_anagrams.py

---
+++ easy/group_anagrams.py
@@ -0,0 +1,19 @@
+class Solution:
+    # O(n) space O(n*mlogm) where n is the number of words and m is the average     
+    # word length
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        sorted_words = {}
+        grouped_anagrams = []
+        
+        for word in strs:
+            sorted_word_list = sorted(word)
+            sorted_word = "".join(sorted_word_list)
+            if sorted_word not in sorted_words.keys():
+                sorted_words[sorted_word] = [word]
+            else:
+                sorted_words[sorted_word].append(word)
+                
+        for key in sorted_words.keys():
+            grouped_anagrams.append(sorted_words[key])
+                
+        return grouped_anagrams
