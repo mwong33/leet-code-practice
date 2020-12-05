d62c2377e2e3bff33b5d559822102e3d8d510a44
Matthew James Wong <matthew.wong76@gmail.com>
2020-12-04 20:33:00 -0800

    Update group_anagrams.py

--- easy/group_anagrams.py
+++ easy/group_anagrams.py
@@ -1,7 +1,7 @@
 class Solution:
     # O(n*m) space O(n*mlogm) where n is the number of words and m is the average     
     # word length
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+    def groupAnagramsBySorting(self, strs: List[str]) -> List[List[str]]:
         sorted_words = {}
         
         for word in strs:
@@ -12,3 +12,19 @@ class Solution:
                 sorted_words[sorted_word_tuple].append(word)
                 
         return sorted_words.values()
+    
+    # O(n*m) space O(n*m) where n is the number of words and m is the average     
+    # word length
+    def groupAnagramsByCount(self, strs: List[str]) -> List[List[str]]:
+        anagrams_by_count = {}
+        
+        for word in strs:
+            count = [0] * 26
+            for letter in word:
+                count[ord(letter)-97] += 1
+            if tuple(count) not in anagrams_by_count.keys():
+                anagrams_by_count[tuple(count)] = [word]
+            else:
+                anagrams_by_count[tuple(count)].append(word)
+                
+        return anagrams_by_count.values()
