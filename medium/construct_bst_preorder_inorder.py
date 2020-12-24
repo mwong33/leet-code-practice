122cdd2328731353610c5e31eff06e80240fedd5
Matthew James Wong <matthew.wong76@gmail.com>
2020-12-24 13:00:39 -0800

    Update construct_bst_preorder_inorder.py

--- medium/construct_bst_preorder_inorder.py
+++ medium/construct_bst_preorder_inorder.py
@@ -5,7 +5,7 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    # O(n^2) time O(n^2) space
+    # O(n^2) time O(n) space
     def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
         return self.buildTreeHelper(0, 0, len(inorder) - 1, preorder, inorder)
     
