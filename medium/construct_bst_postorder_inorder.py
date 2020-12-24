4cac6b3d8de1dccba13f9e74f2b500682a1c9bbc
Matthew James Wong <matthew.wong76@gmail.com>
2020-12-24 12:53:43 -0800

    Update construct_bst_postorder_inorder.py

--- medium/construct_bst_postorder_inorder.py
+++ medium/construct_bst_postorder_inorder.py
@@ -5,7 +5,7 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    # O(n^2) time O(n) space (where n is the number of nodes to construct the tree we are returning)
+    # O(n^2) time O(n^2) space
     def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
         return self.buildTreeHelper(len(postorder)-1, 0, len(inorder)-1, inorder, postorder)
     
