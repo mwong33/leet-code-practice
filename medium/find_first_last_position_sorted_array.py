c676485428b8d3217086835e75071ccd922d94d2
Matthew James Wong <matthew.wong76@gmail.com>
2020-12-29 18:10:33 -0800

    Update find_first_last_position_sorted_array.py

--- medium/find_first_last_position_sorted_array.py
+++ medium/find_first_last_position_sorted_array.py
@@ -11,3 +11,59 @@ class Solution:
                 output[1] = i
             
         return output
+    
+    # O(logn) time O(1) space
+    # Binary Search the higher element and smaller element
+    def searchRangeBinary(self, nums: List[int], target: int) -> List[int]:
+        output = [-1, -1]
+        
+        if len(nums) == 0: 
+            return output
+        
+        if nums[0] == target: 
+            output[0] = 0
+        else:
+            # Binary Search Lower Bound
+            output[0] = self.binarySearchLessThan(nums, target)
+            
+        if nums[len(nums)-1] == target:
+            output[1] = len(nums) - 1
+        else:        
+            # Binary Search Upper Bound
+            output[1] = self.binarySearchGreaterThan(nums, target)
+            
+        return output
+    
+    def binarySearchLessThan(self, nums, target):
+        index = -1
+        start = 0
+        end = len(nums) -1
+        
+        while start <= end:
+            middle = (end + start)//2
+            
+            if nums[middle] == target:
+                index = middle
+            
+            if nums[middle] >= target:
+                end = middle - 1
+            elif nums[middle] < target:
+                start = middle + 1
+        
+        return index
+    
+    def binarySearchGreaterThan(self, nums, target):
+        index = -1
+        start = 0
+        end = len(nums) -1
+        
+        while start <= end:
+            middle = (end + start)//2
+            if nums[middle] == target:
+                index = middle
+            
+            if nums[middle] > target:
+                end = middle - 1
+            elif nums[middle] <= target:
+                start = middle + 1
+        return index
