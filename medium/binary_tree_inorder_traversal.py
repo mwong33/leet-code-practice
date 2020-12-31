0ceec39516154241c01bc287377c1bea2e7185fa
Matthew James Wong <matthew.wong76@gmail.com>
2020-12-31 12:11:17 -0800

    Update binary_tree_inorder_traversal.py

--- medium/binary_tree_inorder_traversal.py
+++ medium/binary_tree_inorder_traversal.py
@@ -16,3 +16,33 @@ class Solution:
         right_sub_tree = self.inorderTraversalRecursion(root.right)
         
         return left_sub_tree + [root.val] + right_sub_tree
+    
+    # O(n) time O(n) space
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        # inorder traversal - left, node, right
+        # We can traverse the tree with DFS via a stack
+        # 1) First keep adding left nodes to the stack.
+        # 2) Once you cannot add anymore left nodes pop the stack and add it to our
+        #    output array
+        # 3) Add the right node to the stack if there is one and repeat step 1)
+        # 4) If there is no right node to the stack, go to step 2
+        if root == None:
+            return []
+        
+        output_array = []
+        stack = []
+        seen = set()
+        stack.append(root)
+        
+        while len(stack) != 0:
+            current_node = stack[len(stack)-1]
+            if current_node.left != None and current_node.left not in seen:
+                stack.append(current_node.left)
+            else:
+                stack.pop()
+                seen.add(current_node)
+                output_array.append(current_node.val)
+                if current_node.right != None:
+                    stack.append(current_node.right)
+                    
+        return output_array
