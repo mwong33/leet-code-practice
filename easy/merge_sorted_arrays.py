3d6c467317aec7e99cc3d84cb7fa1eed5180bddc
Matthew James Wong <matthew.wong76@gmail.com>
2021-01-01 15:31:47 -0800

    Update merge_sorted_arrays.py

--- easy/merge_sorted_arrays.py
+++ easy/merge_sorted_arrays.py
@@ -23,3 +23,28 @@ class Solution:
                 nums1[nums1_index] = nums2[nums2_index]
                 nums1_index += 1
                 nums2_index += 1
+                
+    # O(m) time and O(1) space
+    def mergeNoSort(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        # Start from the end
+        m -= 1
+        n -= 1
+        
+        index = len(nums1) - 1
+        
+        while index >= 0:
+            if m >= 0 and n >= 0:
+                if nums1[m] >= nums2[n]:
+                    nums1[index] = nums1[m]
+                    m -= 1
+                else:
+                    nums1[index] = nums2[n]
+                    n -= 1
+            elif n >= 0:
+                nums1[index] = nums2[n]
+                n -= 1
+                
+            index -= 1
