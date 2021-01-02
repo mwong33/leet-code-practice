bfb2631b5c48b7290df27d7ed5a2dfb2c3d9378f
Matthew James Wong <matthew.wong76@gmail.com>
2021-01-01 16:52:13 -0800

    Update is_palindrome.py

--- easy/is_palindrome.py
+++ easy/is_palindrome.py
@@ -22,3 +22,30 @@ class Solution:
                 end -= 1
                 
         return True
+    
+    # O(n) time O(1) space
+    def isPalindromeSetSolution(self, s: str) -> bool:
+        if len(s) == 0:
+            return True
+        
+        start = 0
+        end = len(s) - 1
+        the_set = {"0","1","2","3","4","5","6","7","8","9",
+                   "a","b","c","d","e","f","g","h","i","j",
+                   "k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"}
+        
+        while start <= end:
+            start_char = s[start].lower()
+            end_char = s[end].lower()
+            
+            if start_char not in the_set:
+                start += 1
+            elif end_char not in the_set:
+                end -= 1
+            else:
+                if start_char != end_char:
+                    return False
+                start += 1
+                end -= 1
+                
+        return True
