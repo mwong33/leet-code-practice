1995a96d25b0070961f4203807c5a90ae749bac5
Matthew James Wong <matthew.wong76@gmail.com>
2021-01-01 21:17:00 -0800

    Update has_cycle.py

--- easy/has_cycle.py
+++ easy/has_cycle.py
@@ -18,3 +18,32 @@ class Solution:
             head = head.next
             
         return False
+    
+    # O(n) time O(1) space
+    def hasCycleTwoPointers(self, head: ListNode) -> bool:
+        if head == None:
+            return False
+        
+        if head.next == None:
+            return False
+        
+        if head.next.next == None:
+            return False
+        
+        slow_pointer = head.next
+        fast_pointer = head.next.next
+        
+        while slow_pointer != None:
+            if slow_pointer == fast_pointer:
+                return True
+            
+            slow_pointer = slow_pointer.next
+            
+            if fast_pointer.next == None:
+                return False
+            if fast_pointer.next.next == None:
+                return False
+            
+            fast_pointer = fast_pointer.next.next
+            
+        return False
