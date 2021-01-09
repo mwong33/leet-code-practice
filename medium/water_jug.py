47307999aa7b3b040a8c5ab0cbb2f187683a3286
Matthew James Wong <matthew.wong76@gmail.com>
2021-01-08 21:32:00 -0800

    Update water_jug.py

--- medium/water_jug.py
+++ medium/water_jug.py
@@ -3,10 +3,26 @@ class Solution:
         if z == 0 or x == z or y == z or x + y == z:
             return True
         
+        if x == 0 or y == 0:
+            return False
+        
         if z > x + y:
             return False
         
-        if z % gcd(x,y) == 0:
+        print(self.greatestCommonDivisor(x,y))
+        
+        if z % self.greatestCommonDivisor(x,y) == 0:
             return True
         
         return False
+    
+    def greatestCommonDivisor(self, x, y):
+        big = max(x,y)
+        small = min(x,y)
+        
+        if small == 0:
+            return big
+        
+        big = big%small
+        
+        return self.greatestCommonDivisor(big, small)
