a6b3ad56747bd1214bc1f3e8586a11a528a2b8d9
Matthew James Wong <matthew.wong76@gmail.com>
2021-01-08 18:32:44 -0800

    Create max_stack.py

---
+++ easy/max_stack.py
@@ -0,0 +1,38 @@
+# O(1) for push, pop, top and O(n) for peekMax and popMax. O(n) space.
+class MaxStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        
+    def pop(self) -> int:
+        return self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[len(self.stack)-1]
+
+    def peekMax(self) -> int:
+        return max(self.stack)
+        
+    def popMax(self) -> int:
+        max_value = max(self.stack)   
+        for i in range(len(self.stack)-1, -1, -1):
+            if self.stack[i] == max_value:
+                self.stack.pop(i)
+                break
+                                
+        return max_value
+
+
+# Your MaxStack object will be instantiated and called as such:
+# obj = MaxStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.peekMax()
+# param_5 = obj.popMax()
