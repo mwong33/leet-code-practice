eaee8f0a33813ecc7968b886e246baf0f4a6d970
Matthew James Wong <matthew.wong76@gmail.com>
2021-01-14 13:58:32 -0800

    Update min_cost_climbing_stairs.py

--- easy/min_cost_climbing_stairs.py
+++ easy/min_cost_climbing_stairs.py
@@ -10,3 +10,17 @@ class Solution:
             second_option = temp
             
         return min(first_option, second_option)
+    
+    # O(n) time O(n) space
+    def minCostClimbingStairsTable(self, cost: List[int]) -> int:
+        if len(cost) == 2:
+            return min(cost)
+        
+        dp = [0] * len(cost)
+        dp[0] = cost[0]
+        dp[1] = cost[1]
+         
+        for index in range(2, len(cost)):
+            dp[index] = min(dp[index-1], dp[index-2]) + cost[index]
+            
+        return min(dp[-1], dp[-2])
