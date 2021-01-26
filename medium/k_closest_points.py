1b81dfee7314315878b718fa3d03d11c36176541
Matthew James Wong <matthew.wong76@gmail.com>
2021-01-26 11:00:37 -0800

    Update k_closest_points.py

--- medium/k_closest_points.py
+++ medium/k_closest_points.py
@@ -1,4 +1,30 @@
 class Solution:
+    # Heap O(nlogK) time O(n) space
+    def kClosestHeap(self, points: List[List[int]], K: int) -> List[List[int]]:
+        heap = []
+        for point in points:
+            if len(heap) < K:
+                heapq.heappush(heap, (-1*self.distance(point), point))
+            else:
+                if self.distance(point) < (-1*heap[0][0]):
+                    heapq.heappop(heap)
+                    heapq.heappush(heap, (-1*self.distance(point), point))
+                    
+        output = []
+        while len(heap) > 0:
+            output.append(heapq.heappop(heap)[1])
+        
+        return output
+    
+    def distance(self, point):
+        return (point[0]**2 + point[1]**2)**0.5
+        
+"""
+[[3, 3], [5, -1], [-2, 4]]
+
+sqrt(18), sqrt(26), sqrt(8)
+"""
+    
     # Sorting and Dictionary - O(nlogn) time O(n) space
     def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
         distance_dict = {}
@@ -28,3 +54,5 @@ class Solution:
                     index += 1
         
         return output
+    
+    import heapq
