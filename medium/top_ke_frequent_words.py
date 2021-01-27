c7616e56cb3a58169683343b933ff3e3867fb17a
Matthew James Wong <matthew.wong76@gmail.com>
2021-01-26 19:05:26 -0800

    Create top_ke_frequent_words.py

---
+++ medium/top_ke_frequent_words.py
@@ -0,0 +1,24 @@
+import heapq
+
+class Solution:
+    # Dictionary + Heap - O(words + k * log(words)) time O(words) space
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        pass
+        word_dict = {}
+        heap = []
+        
+        for word in words:
+            if word not in word_dict:
+                word_dict[word] = 1
+            else:
+                word_dict[word] += 1
+                
+        for word, count in word_dict.items():
+            heapq.heappush(heap, (-1*count, word))
+                       
+        output = []
+        
+        for _ in range(k):
+            output.append(heapq.heappop(heap)[1])
+        
+        return output
