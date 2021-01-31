4ffd1c7ec49dce0323faac46409a6d5ac53028e8
Matthew James Wong <matthew.wong76@gmail.com>
2021-01-30 16:04:56 -0800

    Create path_sum_2.py

---
+++ medium/path_sum_2.py
@@ -0,0 +1,45 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    # O(n^2) O(h) space where h is the height of the tree and n is the number of nodes
+    def pathSum(self, root: TreeNode, targetSum: int) -> List[List[int]]:
+        if root == None:
+            return []
+        
+        return self.dfs(root, targetSum, 0)
+        
+    def dfs(self, root, target_sum, current_sum):
+        current_sum += root.val
+        
+        # Base Case
+        if root.left == None and root.right == None:
+            if current_sum == target_sum:
+                return [[root.val]]
+            else:
+                return []
+        
+        left_result = []
+        right_result = []
+        
+        if root.left != None and root.right != None:
+            left_result = self.dfs(root.left, target_sum, current_sum)
+            right_result = self.dfs(root.right, target_sum, current_sum)
+        elif root.left != None:
+            left_result = self.dfs(root.left, target_sum, current_sum)
+        else:
+            right_result = self.dfs(root.right, target_sum, current_sum)
+            
+        if left_result == [] and right_result == []:
+            return []
+        
+        for index in range(len(left_result)):
+            left_result[index] = [root.val] + left_result[index]
+        
+        for index in range(len(right_result)):
+            right_result[index] = [root.val] + right_result[index]
+            
+        return left_result + right_result
